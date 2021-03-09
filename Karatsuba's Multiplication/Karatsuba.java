2e43db8149269e29f1c82567236f1d5de207b8b9
Tahmid Chowdhury <tahmid12102@live.com>
2021-03-09 09:25:25 -0600

    Moved improvements comment to top

--- Karatsuba's Multiplication/Karatsuba.java
+++ Karatsuba's Multiplication/Karatsuba.java
@@ -1,51 +1,53 @@
 import java.lang.*;
 import java.util.*;
 
+//Improvements: Handle integers beyond long's limit 2^64 -1.
+
 class Karatsuba {
 
-    // recursive algorithm function
+    //Recursive algorithm method.
     public static long karatsubaAlgo (long x, long y) {
         
         int maxLength;
 
-        //Get max num length between x and y
+        //Get maximum number length between x and y.
         if (length(x) < length(y)){maxLength = length(y);}
         else {maxLength = length(x);}
 
-        //Base case:
+        //Base case
         if (maxLength <= 1){
             return x * y;
         }
         
-        //Divide maxLength by 2 and add 1 if odd
+
+        //Divide maxLength by 2 and add 1 if odd.
         maxLength = (maxLength / 2) + (maxLength % 2);
 
-        //Create multiplier by taking 10^(maxLength rounded up)
+        //Create multiplier by taking 10^(maxLength) to pad ad_bc with 0's
         long multiplier = (long)Math.pow(10, (maxLength));
 
-        //compute a,b,c,d by using string manipulations or clever multiplier division
-        // a = x/multiplier , ignore remainder
-        // b = x mod multplier , receive remainder
-        // c = y /multiplier, ignore remainder
-        // d = y mod multiplier, receive remainder
+        //Compute a,b,c,d by using multiplier. 
+        // a = x / multiplier, ignoring remainder.
+        // b = x % multplier , receiving remainder
+        // c = y / multiplier, ignoring remainder.
+        // d = y % multiplier, receiving remainder.
         long a = x / multiplier;
         long b = x % multiplier;
         long c = y / multiplier;
         long d = y % multiplier;
 
-        //Compute ac, bd, (a+b)(c+d) recursively
+        //Compute ac, bd, and (a+b)(c+d) recursively.
         long ac = karatsubaAlgo(a,c);
         long bd = karatsubaAlgo(b,d);
         long ac_ad_bc_bd = karatsubaAlgo(a+b,c+d);
         long ad_bc = ac_ad_bc_bd - ac - bd;
 
-        //return  ac*10^(multiplier * 2   i.e. beacuse its halved) + bd + (((a+b)(c*d)-ac-bd) * multiplier(already halved))
+        //Return final result.
         return (ac * (long)Math.pow(10, maxLength *2)) + bd + (ad_bc * multiplier);
     }
 
-    //Custom class that returns length of digit using repeated multiplication.
+    //Method returns length of digit using repeated multiplication.
     //Simple and faster than converting to string or using logarithmic approach.
-
     public static int length(long n) {
         int length = 0;
         long temp = 1;
@@ -56,7 +58,7 @@ class Karatsuba {
         return length;
     }
     
-    //Main function for test cases
+    //Main function for test cases.
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two values to multiply: "); 
@@ -64,6 +66,4 @@ class Karatsuba {
         long y = Long.parseLong(sc.nextLine());
         System.out.println("The product between the two is: " + karatsubaAlgo(x,y));
     }
-
-    //Improvements: Handle numbers that can go beyond long's limit 2^64 -1.
 }
